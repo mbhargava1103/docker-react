70c9f1d409bf52795640828a6465528c7159e0ba
Mayank Bhargava <max.march11@gmail.com>
2023-09-30 21:07:23 +0800

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link 1', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2zxz3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2zxz3ssdsds', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2zxz3ssdsds', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
